8dad654d579750f22d0a40a686fec3ae5d7d31a8
tboenig <mschulz@bbaw.de>
2022-04-21 11:52:18 +0200

    Update readmefolder.sh

--- scripts/readmefolder.sh
+++ scripts/readmefolder.sh
@@ -6,10 +6,11 @@ then
     echo "$DIR and readme.old are exists..."
 else
 	mkdir readme
-    mv readme.md readme/readme.old
+    mv README.md readme/README.old
     git config --local user.email "41898282+github-actions[bot]@users.noreply.github.com"
     git config --local user.name "github-actions[bot]"
     git add ./readme/*
     git commit -m "[Automatic] Update readme.old files" || echo "Nothing to update"
+    touch README.xml
 
 fi
