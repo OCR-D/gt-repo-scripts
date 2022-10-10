01f7b1c7f16cfdc6c64f56ee2ed28dc00820f35b
Matthias Boenig <mschulz@bbaw.de>
2022-10-10 09:25:23 +0200

    Update readmefolder.sh

--- scripts/readmefolder.sh
+++ scripts/readmefolder.sh
@@ -5,7 +5,7 @@ if [ -d "$DIR" ];
 then
     echo "$DIR and readme.old are exists..."
 else
-	mkdir readme_old
+    mkdir readme_old
     mv README.md readme_old/README.md
     git config --local user.email "41898282+github-actions[bot]@users.noreply.github.com"
     git config --local user.name "github-actions[bot]"
